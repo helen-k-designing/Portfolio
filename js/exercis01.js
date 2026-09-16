fb9fc86ce968b7969ab3a3cbd82c67729d8c505c
Helen-k-designing <helen.k.designing@gmail.com>
2026-09-16 08:57:17 +0300

    Portfolio

---
+++ js/exercis01.js
@@ -0,0 +1,65 @@
+'use strict';
+
+
+let message1 = "Hello";
+let message2 = "JavaScript!";
+alert(message1 + " " + message2);
+
+
+/*
+let x = prompt("Введіть перше число (x):");
+let y = prompt("Введіть друге число (y):");
+let sum = Number(x) + Number(y);
+alert("Сума чисел: " + sum);
+*/
+
+/*
+let user_number = Number(prompt("Вгадайте число від 1 до 10:"));
+if (user_number === 3) {
+    alert('Congratulations, You did it!'); }
+else if (user_number > 3) {
+    alert('The number is too long'); }
+else if (user_number < 3) {
+    alert('The number is short'); }
+*/
+
+/*
+let userDay = prompt("Введіть назву дня тижня (наприклад, Понеділок або Monday):");
+if (userDay === "Понеділок" || userDay === "Monday") {
+    alert("Start of the work week!"); } 
+else if (userDay === "П'ятниця" || userDay === "Friday") {
+    alert("End of the work week!"); } 
+else {
+   alert("A regular day"); }
+*/
+/*
+let userDay = prompt("Введіть назву дня тижня (наприклад, Понеділок або Monday):");
+switch (userDay) {
+    case "Понеділок":
+    case "Monday":
+        alert("Start of the work week!");
+        break;
+    case "П'ятниця":
+    case "Friday":
+        alert("End of the work week!");
+        break;
+    default:
+        alert("A regular day");
+        break;}
+*/       
+
+/*
+let score = Number(prompt("Введіть кількість балів студента (від 0 до 100):"));
+let grade;
+if (score < 50) {
+    grade = 'F'; } 
+else if (score < 70) {
+    grade = 'D'; } 
+else if (score < 80) {
+    grade = 'C'; } 
+else if (score < 90) {
+    grade = 'B'; } 
+else {
+    grade = 'A'; }
+alert("Ступінь студента: " + grade);
+*/
